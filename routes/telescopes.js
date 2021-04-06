ddfae8f0c48fb9859c0e2b878ab3d9809724d0c9
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-04-06 08:32:20 +0000

    update function implemented

--- routes/telescopes.js
+++ routes/telescopes.js
@@ -5,7 +5,7 @@ const {Telescope} = require("../models")
 
 const {bootstrapField, createTelescopeForm} = require('../forms')
 //rendering full product list
-router.get('/', async (req,res)=>{
+router.get('/telescope', async (req,res)=>{
     let telescopes = await Telescope.collection().fetch()
     res.render('telescopes/index', {
         'telescopes':telescopes.toJSON()
@@ -27,12 +27,40 @@ router.post('/telescope/create', async (req,res)=>{
             const telescope = new Telescope()
             telescope.set(form.data)
             await telescope.save()
+            res.redirect('/telescope')
         },
         'error': async (form) => {
             res.render('telescopes/create', {
                 'form': form.toHTML(bootstrapField)
             })
         }
+    })
+})
+
+router.get('/telescope/:telescope_id/update', async (req,res)=>{
+    const telescopeId = req.params.telescope_id
+    const telescope = await Telescope.where({
+        'id':telescopeId
+    }).fetch({
+        require:true
+    })
+
+    const telescopeForm = createTelescopeForm()
+
+    telescopeForm.fields.name.value = telescope.get('name')
+    telescopeForm.fields.description.value = telescope.get('description')
+    telescopeForm.fields.stock.value = telescope.get('stock')
+    telescopeForm.fields.price.value = telescope.get('price')
+    telescopeForm.fields.weight.value = telescope.get('weight')
+    telescopeForm.fields.userLevel.value = telescope.get('userLevel')
+    telescopeForm.fields.imagingType.value = telescope.get('imagingType')
+    telescopeForm.fields.opticalDesign.value = telescope.get('opticalDesign')
+    telescopeForm.fields.apertureRange.value = telescope.get('apertureRange')
+    telescopeForm.fields.fratioRange.value = telescope.get('fratioRange')
+
+    res.render('telescopes/update', {
+        'form': telescopeForm.toHTML(bootstrapField),
+        'telescope':telescope.toJSON()
     })
 })
 
