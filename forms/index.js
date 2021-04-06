54c24a372d952440940fdacd33d63c06adea8cdb
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-04-06 07:46:55 +0000

    added forms for telescope listing creation

---
+++ forms/index.js
@@ -0,0 +1,107 @@
+const forms = require("forms")
+const fields = forms.fields
+const validators = forms.validators;
+var widgets = require('forms').widgets;
+
+var bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var validationclass = object.value && !object.error ? 'is-valid' : '';
+    validationclass = object.error ? 'is-invalid' : validationclass;
+    if (validationclass) {
+        object.widget.classes.push(validationclass);
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="invalid-feedback">' + object.error + '</div>' : '';
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
+
+const createTelescopeForm = () => {
+    return forms.create({
+        'name': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+        }),
+        'description': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+        }),
+        'stock': fields.number({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            'validators':[validators.integer()]
+        }),
+        'price': fields.number({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            'validators':[validators.integer()]
+        }),
+        'weight': fields.number({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            'validators':[validators.integer()]
+        }),
+        'userLevel': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            // widget: widgets.select
+        }),
+        'imagingType': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'opticalDesign': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'apertureRange': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'fratioRange': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        
+        
+    })
+}
+
+module.exports = {bootstrapField, createTelescopeForm}
--- routes/telescopes.js
+++ routes/telescopes.js
@@ -3,10 +3,36 @@ const router = express.Router()
 
 const {Telescope} = require("../models")
 
+const {bootstrapField, createTelescopeForm} = require('../forms')
+//rendering full product list
 router.get('/', async (req,res)=>{
     let telescopes = await Telescope.collection().fetch()
     res.render('telescopes/index', {
         'telescopes':telescopes.toJSON()
+    })
+})
+
+//telescope forms 
+router.get('/telescope/create', async (req,res)=>{
+    const telescopeForm = createTelescopeForm()
+    res.render('telescopes/create',{
+        'form':telescopeForm.toHTML(bootstrapField)
+    })
+})
+
+router.post('/telescope/create', async (req,res)=>{
+    const telescopeForm = createTelescopeForm()
+    telescopeForm.handle(req, {
+        'success': async (form) => {
+            const telescope = new Telescope()
+            telescope.set(form.data)
+            await telescope.save()
+        },
+        'error': async (form) => {
+            res.render('telescopes/create', {
+                'form': form.toHTML(bootstrapField)
+            })
+        }
     })
 })
 
